b2ca0c017a587781bb3b4e4b175f043563bb73aa
禾子尢乂 <abstergoyw@gmail.com>
2022-04-25 22:40:39 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://github.com/gedoor/',
-  baseUrl: '/',
+  baseUrl: '/gedoor.github.io/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
